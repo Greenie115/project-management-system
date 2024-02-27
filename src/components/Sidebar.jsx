b8cf2afd84b66c9178be4234ac49b9d772415615
Greenie115 <dggreen545@gmail.com>
2024-02-27 20:57:30 +0000

    Button compoonent added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
+import Button from "./Button";
+
 function Sidebar (){
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
     return(
         <>
         <menu className="container ">
             <div className="bg-zinc-900 max-w-80 h-screen mt-20 rounded-tr-3xl">
                 <h2 className="text-white p-10">Your Projects</h2>
+                <Button 
+                    content='Add New +'
+                    onClick={handleClick}/>
             </div>
         </menu>
         </>
