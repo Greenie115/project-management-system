65a5cb47d395d3fca8195aa87b116a6c9ba50832
Greenie115 <dggreen545@gmail.com>
2024-02-27 20:50:58 +0000

    Sidebar component addeed

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+function Sidebar (){
+    return(
+        <>
+        <menu className="container ">
+            <div className="bg-zinc-900 max-w-80 h-screen mt-20 rounded-tr-3xl">
+                <h2 className="text-white p-10">Your Projects</h2>
+            </div>
+        </menu>
+        </>
+    )
+};
+
+export default Sidebar;
