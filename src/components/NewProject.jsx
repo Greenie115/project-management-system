b5c0769d1a18cfe596be457d2a3d78efcab3a5e6
Greenie115 <dggreen545@gmail.com>
2024-03-22 19:10:57 +0000

    working new project updating field

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,44 +1,80 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function NewProject() {
+    const [newProject, setNewProject] = useState({
+        title: '',
+        manager: '',
+        description: '',
+        date: ''
+    });
 
-const submitProject = () => {
-    console.log(projectTitle)
-}
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setNewProject(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const submitProject = event => {
+        event.preventDefault();
+        console.log(newProject);
+    }
 
     return (
-        <>
-            <form className="m-1 p-1">
-                <label className="m-1 p-1">
-                    Project Title
-                <input name='projectTitle' type="text" className="m-1 p-1" required/>
-                </label>
-
-                <label className="m-1 p-1">
-                    Manager
-                </label>
-                <input type="text" className="m-1 p-1" required/>
-
-            </form>
-            <form className="m-1 p-1">
-                <label className="m-1 p-1">
-                    Project Description
-                </label>
-                <br />
-                <textarea rows="4" cols="50" className="m-1 p-1" required/>
-            </form>
-            <form >
-            <label className="m-1 p-1" required>
-                    Due Date
-                </label>
-                <input type="date" className="m-1 p-1" required/>
-            </form>
+        <form onSubmit={submitProject} className="m-1 p-1">
+            <label className="m-1 p-1">
+                Project Title
+                <input
+                    name='title'
+                    type="text"
+                    className="m-1 p-1"
+                    value={newProject.title}
+                    onChange={handleInputChange}
+                    required
+                />
+            </label>
+            <label className="m-1 p-1">
+                Manager
+                <input
+                    name='manager'
+                    type="text"
+                    className="m-1 p-1"
+                    value={newProject.manager}
+                    onChange={handleInputChange}
+                    required
+                />
+            </label>
+            <label className="m-1 p-1">
+                Project Description
+                <textarea
+                    name='description'
+                    rows="4"
+                    cols="50"
+                    className="m-1 p-1"
+                    value={newProject.description}
+                    onChange={handleInputChange}
+                    required
+                />
+            </label>
+            <label className="m-1 p-1">
+                Due Date
+                <input
+                    name='date'
+                    type="date"
+                    className="m-1 p-1"
+                    value={newProject.date}
+                    onChange={handleInputChange}
+                    required
+                />
+            </label>
             <Button
                 content="Submit"
-                onClick={submitProject}
+                type="submit"
             />
-        </>
-    )
-};
+        </form>
+    );
+}
 
 export default NewProject;
